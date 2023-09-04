04768ae8b0efd5c5070afe5e9d603ab48c3aca2f
TuanLM <tuanvipandpro@gmail.com>
2023-09-04 17:49:26 +0700

    Init

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
+public class DemoController {
+
+    @GetMapping("/get")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<String>("Welcome to Spring Boot Server !", HttpStatus.OK);
+    } 
+}
